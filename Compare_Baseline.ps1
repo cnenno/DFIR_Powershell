07ae2c612b2284ecbd694f77eaac2828d07fe88a
ThatCyberGuy5 <61467648+cnenno@users.noreply.github.com>
2021-11-07 10:11:53 -0600

    Update Compare_Baseline.ps1
    fixed syntax and grammar

--- Compare_Baseline.ps1
+++ Compare_Baseline.ps1
@@ -4,10 +4,10 @@
 ##################################################
 
 
-###This will take the input of the baseline document and compare it to the Changes document
-###this can be used in conjunction with the DFIR_Host_Baseline_Gathering1 script
-###you will just need to make use the paths and file types line up
-###Then output the differences cleanly, marked by whats different for each document
+### This will take the input of the baseline document and compare it to the changes document
+### This can be used in conjunction with the DFIR_Host_Baseline_Gathering1 script
+### You will just need to make sure the paths and file types line up
+### Then output the differences cleanly, marked by whats different for each document
 
 $c = (Write-Output (Compare-Object (Get-Content $env:HOMEPATH\Baseline.docx)(Get-Content $env:HOMEPATH\Changes.docx) | select -ExpandProperty inputobject -ExcludeProperty outputobject))
 Write-Output $c | Out-file $env:HOMEPATH\OverallEffectiveChanges.docx
