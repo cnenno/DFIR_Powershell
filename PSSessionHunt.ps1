c190174ceb91d2690e2d7bb622b70533931ec1a1
ThatCyberGuy5 <61467648+cnenno@users.noreply.github.com>
2023-01-14 10:14:02 -0600

    Update PSSessionHunt.ps1
    Updates to grammar and syntax for 01/14/2023

--- PSSessionHunt.ps1
+++ PSSessionHunt.ps1
@@ -1,3 +1,5 @@
+## This is a method for setting your policy, then remoting into a remote host, and allowing that connection.
+
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 Netsh firewall set service remoteadmin enable
 enable-PSRemoting -force
